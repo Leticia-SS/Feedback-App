53d9d947302745c7928bd57fa8637fb4f2346055
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-08-12 21:51:44 -0300

    Learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+    <h1>Hello do compnente App</h1> // Só pode retornar um elemento Pai
+    //  <p>Impossível de ter mais um elemento pai</p> 
+    )
 }
 
-export default App;
+// O que funcionaria seria:
+
+// function App() {
+//     return (
+//         <>
+//          <h1>Hello do compnente App</h1>
+//          <p 2 Elementos filhos de uma tag vazia, que ao entrar no html irá sumir</p> 
+//         </>
+//     )
+// }
+
+// Alguns atributos são escritos diferentes no jsx, como class vira clasName, label seria htmlFor
+
+// Algo simples sem JSX, usando puro Javascript:
+// import React from 'react'
+
+// function App() {
+//     return React.createElement('div', { className: 'coitnainer' }, React.createElement('h1', {}, 'My App'))
+// }
+
+export default App
